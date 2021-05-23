f426b2c405f149b202b7590573e5f1ad534f1692
Revathy-2001 <revathyu10@gmail.com>
2021-05-23 19:24:06 +0530

    Create a new user by sending a post request to /users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
-    render plain: User.all.map { |user| user.show_user_details }.join("\n")
+    render plain: User.order(:id).map { |user| user.show_user_details }.join("\n")
+  end
+
+  def create
+    new_user = User.create!(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+    )
+    render plain: "New User created with Id #{new_user.id}"
   end
 end
