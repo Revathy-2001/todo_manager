c834329dff935b7d9b79223987b0410b218442b4
Revathy-2001 <revathyu10@gmail.com>
2021-05-31 13:47:19 +0530

    Render the front end todo app with data's from the database in /todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,6 +3,7 @@ class TodosController < ApplicationController
 
   def index
     #render plain: Todo.order(:id).map { |todo| todo.to_pleasant_string }.join("\n")
+    render "index"
   end
 
   def show
