0190bd9e4e09ca68af2ee76a06a5f1e3b483c950
Revathy-2001 <revathyu10@gmail.com>
2021-06-05 23:27:34 +0530

    Validate todos before saving

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,5 +1,9 @@
 class Todo < ActiveRecord::Base
+  validates :todo_text, presence: true
+  validates :due_date, presence: true
+  validates :todo_text, length: { minimum: 2 }
   belongs_to :user
+
   def self.overdue
     all.where("due_date < ? and (not completed)", Date.today)
   end
