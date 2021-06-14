059778b4f88c4e16dd835aa881169d6603d3078c
Revathy-2001 <revathyu10@gmail.com>
2021-06-14 18:45:18 +0530

    Add simple functionalities to main

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :todos
   resources :users
   get "/" => "home#index"
-  post "users/login", to: "users#login"
   get "signin" => "sessions#new", as: :new_sessions
   post "signin" => "sessions#create", as: :sessions
   delete "signout" => "sessions#destroy", as: :destroy_session
